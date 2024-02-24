2a76546b04b21139aeccf2b5312856385a9086c5
Bryan Sandoval <bsandovalr1@miumg.edu.gt>
2024-02-24 11:28:27 -0600

    primer 1

---
+++ tarea clase 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+//ejercicio1
+int x = 2;
+
+while (x<=8)
+{
+    Console.WriteLine(x);
+    
+    x = x + 2;
+}
+
+//ejercicio 1.2
+for (int i = 0; i <= 10; i++)
+{
+    int suma = 0;
+    suma +=i;
+}
+
+//Ejercicio 2.1
+Console.WriteLine("ingresa numeros enteros positivos: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = n; i <= 10; i++)
+{
+    
+    n += i;
+}
+
